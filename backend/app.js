a5908b5bac28bd4f2eff32ca35b50fbf4361f630
Feifei Zhang <zwz108@gmail.com>
2025-08-01 22:49:14 -0700

    Simplified app.js after filtering out routers and controllers.

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-
-const Thing = require("./models/thing");
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
 
@@ -42,62 +41,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Intercept POST requests
-app.post("/api/stuff", (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
-  // Save the new instance of Thing to database
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Post saved successfully!" }))
-    .catch((err) => res.status(400).json({ error: err })); // Send a 404 error to the front end, along with an error object of the error thrown by Mongoose
-});
-
-// Intercept GET request
-// Use a colon in front of the dynamic segment of the route to make it accessible as a parameter.
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id }) // req.params.xxx where xxx is same as xxx after :
-    .then((thing) => res.status(200).json(thing))
-    .catch((err) => res.status(400).json({ error: err }));
-});
-
-// Intercept PUT request
-app.put("/api/stuff/:id", (req, res, next) => {
-  // Create a new _id field by default.
-  // That would throw an error in this case, as you would be trying to modify an immutable field on a database document.
-  const thing = new Thing({
-    // Therefore, you must use the id parameter from the request to set your Thing up with the same _id as before.
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId,
-  });
-  Thing.updateOne({ _id: req.params.id }, thing)
-    .then(() =>
-      res.status(201).json({ message: "Thing updated successfully!" })
-    )
-    .catch((err) => res.status(400).json({ error: err }));
-});
-
-// Intercept DELETE request
-app.delete("/api/stuff/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Deleted!" }))
-    .catch((err) => res.status(400).json({ error: err }));
-});
-
-app.use("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((err) => res.status(400).json({ error: err }));
-});
+// Register Express router for all requests to /api/stuff (will be sent to stuffRoutes)
+app.use('/api/stuff', stuffRoutes);
 
 // Before we populate our express out, we are going to export it so that we can access it outside this js file.
 module.exports = app;
