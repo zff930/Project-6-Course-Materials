9edaf6a81fe13b7efa48ec075b1037f27a985ca9
Feifei Zhang <zwz108@gmail.com>
2025-08-15 21:13:01 -0700

    Updated stuff routes for authentication middleware

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
 // For every request coming to this endpoint, use the corresponding function on our stuff controller.
-router.post("/", stuffCtrl.createThing);
-router.get("/", stuffCtrl.findAllThings);
-router.get("/:id", stuffCtrl.findOneThing); // Use a colon in front of the dynamic segment of the route to make it accessible as a parameter.
-router.put("/:id", stuffCtrl.updateThing);
-router.delete("/:id", stuffCtrl.deleteThing);
+// Add auth middleware before controller
+router.post("/", auth, stuffCtrl.createThing);
+router.get("/", auth, stuffCtrl.findAllThings);
+router.get("/:id", auth, stuffCtrl.findOneThing); // Use a colon in front of the dynamic segment of the route to make it accessible as a parameter.
+router.put("/:id", auth, stuffCtrl.updateThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
 
 module.exports = router;
