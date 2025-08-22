e12fa1c6e7080217ced0c61d07dd601305ebd094
Feifei Zhang <zwz108@gmail.com>
2025-08-21 22:37:13 -0700

    Checked if req.body is defined just in case GET request doesn't have a req.body.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,21 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.userId;
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const userId = decodedToken.userId;
 
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'Invalid ID';
-        } else {
-            next();
-        }
-    } catch {
-        res.status(401).json({
-            error: new Error('Invalid request!')
-        });
+    if (req.body && req.body.userId && req.body.userId !== userId) {
+      throw "Invalid ID";
+    } else {
+      next();
     }
+  } catch {
+    res.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
 };
-
 
