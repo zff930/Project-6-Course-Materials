c648bfefa9cd80972d94196ebf6746215bf8fdb2
Feifei Zhang <zwz108@gmail.com>
2025-08-29 22:21:03 -0700

    Added a multer to accept incoming files.

---
+++ backend/middleware/multer.config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIMI_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png'
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        // first argument: whether received error, second argument: folder to save file
+        callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({storage: storage}).single('image');
